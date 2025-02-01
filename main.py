296d15d4e157a41b360135a9c03ec0539ec3a5c7
Vlad <vys@jetton.games>
2025-02-02 00:21:38 +0400

    Implement bot

---
+++ main.py
@@ -0,0 +1,49 @@
+import asyncio
+import logging
+import os
+import sys
+
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.types import Message, WebAppInfo
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+WEB_APP_URL = os.getenv("WEB_APP_URL")
+
+dp = Dispatcher()
+
+
+@dp.message()
+async def handle_all_messages(message: Message):
+    keyboard = InlineKeyboardBuilder()
+    text = ("<b>Buy TRX for USDT</b>\n"
+            "Even if you don't have any TRX right now.")
+    keyboard.button(
+        text="Buy TRX",
+        web_app=WebAppInfo(
+            url=WEB_APP_URL,
+        ),
+    )
+    await message.answer(
+        text=text,
+        reply_markup=keyboard.as_markup(),
+        parse_mode=ParseMode.HTML,
+    )
+
+
+async def main() -> None:
+    bot = Bot(
+        token=BOT_TOKEN,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML),
+    )
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
